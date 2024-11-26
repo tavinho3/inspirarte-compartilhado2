91b419c06fea62e1ba06f811e8fcec6987016afa
tavinho3 <gustavo.araujo170@etec.sp.gov.br>
2024-11-26 00:37:25 -0300

    Add files via upload

---
+++ Tela inicial/JS/Tela inicial.js
@@ -0,0 +1,36 @@
+function trocadetela(){
+    window.location.href = "../../Tela de historia/HTML/tela de historia.html"
+   }
+
+   document.getElementById('menu-toggle').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('hidden')
+    });
+
+    document.getElementById('banner').addEventListener('click', function() {
+        window.location.href = "../../Tela de historia/HTML/tela de historia.html"
+    } );
+
+    function teladeperfil(){
+        window.location.href= "../../tela de perfil/HTML/tela de perfil.html"
+    }
+
+    function teladesobre(){
+        window.location.href= "../../Tela de sobre nos/HTML/sobre.html";
+    }
+
+    function teladecategorias(){
+        window.location.href= "../../Tela de categorias/HTML/categorias.html";
+    }
+
+    function teladelogin(){
+        window.location.href= "../../Tela de login/HTML/login2.html";
+    }
+
+    function teladecadastro(){
+        window.location.href= "../../Tela de cadastro/HTML/cadstro.html";
+    }
+
+    function teladecriaçãodehistoria(){
+        window.location.href= "../../Tela de criação de historia/HTML/Tela de criação de historia.html";
+    }
